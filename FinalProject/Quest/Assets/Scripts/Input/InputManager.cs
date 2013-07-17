a91b85fd8302f9ba55646a6a537729bae2f017ea
Jeffery Myers <jeffm2501@gmail.com>
2013-07-16 23:11:27 -0700

    start movement

--- FinalProject/Quest/Assets/Scripts/Input/InputManager.cs
+++ FinalProject/Quest/Assets/Scripts/Input/InputManager.cs
@@ -5,13 +5,27 @@ public class InputManager : MonoBehaviour
 {
     public static InputManager Instance;
 
+    GameObject PlayerObject;
+
+    Movement PlayerMovemnt;
+
 	void Start ()
 	{
-        Instaince = this;
+        Instance = this;
 	}
-	
+
+    public void SetPlayer(GameObject player)
+    {
+        PlayerObject = player;
+        PlayerMovemnt = player.GetComponent("Movement") as Movement;
+    }
+
 	void Update ()
 	{
-	
+        if (PlayerObject == null)
+            return;
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 	}
 }
